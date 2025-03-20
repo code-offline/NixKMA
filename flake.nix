785d29844e70432cd3c1ad2567c8267653381f6d
Gershaveut <vova1234567890012011@yandex.ru>
2025-03-20 17:33:33 +0300

    изменение flake

--- flake.nix
+++ flake.nix
@@ -3,15 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     system = "x86_64-linux";
+    homeStateVersion = "24.11";
     stateVersion = "24.11";
     user = "student";
     hostname = "nixKMA";
   in {
-    nixosConfigurations.nixKMA = inputs.nixpkgs.lib.nixosSystem {
+    nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
       inherit system;
       
       modules = [
@@ -21,6 +27,17 @@
       specialArgs = {
         inherit inputs stateVersion user hostname;
       };
+    };
+
+    homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        inherit inputs homeStateVersion user;
+      };
+
+      modules = [
+        ./home-manager/home.nix
+      ];
     };
   };
 }
