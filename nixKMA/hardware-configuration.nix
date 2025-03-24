85e55c1e9a3350059d32eba261050d2e3dbb607b
Gershaveut <vova1234567890012011@yandex.ru>
2025-03-24 16:31:58 +0300

    исправление

--- nixKMA/hardware-configuration.nix
+++ nixKMA/hardware-configuration.nix
@@ -1,11 +1,11 @@
 {
   fileSystems."/" = { 
-    device = "/dev/sda2";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/sda1";
+    device = "/dev/disk/by-label/NIXOS-BOOT";
     fsType = "vfat";
     options = [ "fmask=0022" "dmask=0022" ];
   };
