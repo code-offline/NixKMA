9b12ab64758efce5a8a804c81f844b66e4e779f0
Gershaveut <vova1234567890012011@yandex.ru>
2025-03-25 12:51:58 +0300

    исправление

--- nixKMA/modules/boot.nix
+++ nixKMA/modules/boot.nix
@@ -1,11 +1,13 @@
 {
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    useOSProber = true;
-    device = "nodev";
-    default = 2;
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      useOSProber = true;
+      device = "nodev";
+      default = 2;
+    };
     timeout = 10;
+    efi.canTouchEfiVariables = true;
   };
-  boot.loader.efi.canTouchEfiVariables = true;
 }
